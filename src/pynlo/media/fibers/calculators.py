87922a3a40598c0313f790d86f583df563843637
gregmoille <greg.moille@gmail.com>
2019-01-25 13:53:16 -0500

    Calculator β through dispersion with frequency instead of wavlength (needed sa λ scale not linear)

--- src/pynlo/media/fibers/calculators.py
+++ src/pynlo/media/fibers/calculators.py
@@ -29,56 +29,58 @@ from scipy import constants
 import matplotlib.pyplot as plt
 
 def DTabulationToBetas(lambda0, DData, polyOrder, DDataIsFile = True, return_diagnostics = False):
-    """ Read in a tabulation of D vs Lambda. Returns betas in array 
+    """ Read in a tabulation of D vs Lambda. Returns betas in array
     [beta2, beta3, ...]. If return_diagnostics is True, then return
     (betas, fit_x_axis (omega in THz), data (ps^2), fit (ps^2) ) """
-    # 
+    #
     # Expand about lambda0
     makePlots = 0
     if DDataIsFile:
         DTab = np.genfromtxt(DData,delimiter=',',skiprows=1)
     else:
         DTab = DData[:]
-            
+
     # Units of D are ps/nm/km
-    # Convert to s/m/m 
+    # Convert to s/m/m
     DTab[:,1] = DTab[:,1] * 1e-12 * 1e9 * 1e-3
+    ν = DTab[:,0]*1e12
+    ω = 2*np.pi*ν
     c = constants.speed_of_light
-    
-    omegaAxis = 2*np.pi*c  / (DTab[:,0]*1e-9) - 2*np.pi*c  /(lambda0 * 1e-9)
-    # Convert from D to beta via  beta2 = -D * lambda^2 / (2*pi*c) 
-    
-    betaTwo = -DTab[:,1] * (DTab[:,0]*1e-9)**2 / (2*np.pi*c) 
+
+    # 1/w  = (lbd/2*bc)
+    omegaAxis = ω - 2*np.pi*c  /(lambda0 * 1e-9)
+    # Convert from D to beta via  beta2 = -D * lambda^2 / (2*pi*c)
+    betaTwo = -DTab[:,1] * 2*np.pi*c/ω**2
     # The units of beta2 for the GNLSE solver are ps^2/m; convert
     betaTwo = betaTwo * 1e24
     # Also convert angular frequency to rad/ps
     omegaAxis = omegaAxis * 1e-12 #  s/ps
-    
+
     # How betas are interpreted in gnlse.m:
     #B=0;
     #for i=1:length(betas)
     #    B = B + betas(i)/factorial(i+1).*V.^(i+1);
     #end
-    
+
     # Fit beta2 with high-order polynomial
     polyFitCo = np.polyfit(omegaAxis, betaTwo, polyOrder)
-    
+
     Betas = polyFitCo[::-1]
-    
-    polyFit = np.zeros((len(omegaAxis),))   
+
+    polyFit = np.zeros((len(omegaAxis),))
 
     for i in range(len(Betas)):
         Betas[i] = Betas[i] * factorial(i)
         polyFit = polyFit + Betas[i] / factorial(i)*omegaAxis**i
-    
-    if makePlots == 1:
+
+    if makePlots == 0:
 #        try:
 #            set(0,'CurrentFigure',dispfig);
 #        catch ME
 #            dispfig = figure('WindowStyle', 'docked');
-#        end        
-        plt.plot(omegaAxis, betaTwo,'o')
-        plt.plot(omegaAxis, polyFit)
+#        end
+        plt.plot(ν*1e-12, betaTwo,'o')
+        plt.plot(ν*1e-12, polyFit)
         plt.show()
     if return_diagnostics:
         return Betas, omegaAxis, betaTwo, polyFit
